2c240d048d1d4d955904204334e36d158b991508
Bruno Bastos <bastosbrunods@gmail.com>
2023-05-09 01:55:43 -0300

    Update Program.cs

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 
+/* PRATICANDO POLIMORFISMO*/
+
 class Program
 {
 
     Adm gerente = new Adm("gerencia", "Bruno", "bruno@gmail.com", 123);
-
 }
 
 class Usuarios
@@ -19,19 +20,32 @@ class Usuarios
         this._email = email;
         this._senha = senha;
     }
+    
+    //VIRTUALIZAÇÃO DE UM METODO PARA PERMITIR A SOBRESCRITA DO MESMO:
+    public virtual void ExibirDados()
+    {
+        Console.WriteLine($"NOME: {_nome}");
+        Console.WriteLine($"EMAIL: {_email}");
+        Console.WriteLine($"SENHA: {_senha}");
+    }
 }
 
 class Adm : Usuarios
 {
     public string ? _setor;
 
+    //HERANAÇ COM CONSTRUTOR:
     public Adm(string setor, string nome, string email, int senha) : base(nome, email, senha)
     {
         this._setor = setor;
-    } 
-}
+    }
 
-class Funcionarios
-{
-
+    //SOBRESCRITA DO METODO VIRTUALIZADO DA CLASS PAI:
+    public override void ExibirDados()
+    {
+        Console.WriteLine("DADOS ADM:");
+        base.ExibirDados();
+        Console.WriteLine($"SENHA: {_setor}");
+        
+    }
 }
