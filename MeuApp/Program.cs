b5be6fc94f6b2355ca5a7bbffb817c8348f33bed
Bruno Bastos <bastosbrunods@gmail.com>
2023-05-09 01:16:53 -0300

    Update Program.cs

--- MeuApp/Program.cs
+++ MeuApp/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+
+    Adm gerente = new Adm("gerencia", "Bruno", "bruno@gmail.com", 123);
+
+}
+
+class Usuarios
+{
+    public string ? _nome;
+    public string ? _email;
+    public int ? _senha;
+
+    public Usuarios(string nome, string email, int senha)
+    {
+        this._nome = nome;
+        this._email = email;
+        this._senha = senha;
+    }
+}
+
+class Adm : Usuarios
+{
+    public string ? _setor;
+
+    public Adm(string setor, string nome, string email, int senha) : base(nome, email, senha)
+    {
+        this._setor = setor;
+    } 
+}
+
+class Funcionarios
+{
+
+}
